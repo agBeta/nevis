a3fbf1a69e773cbe9f632de2f79a78b634c5156a
agBeta <alirezaghorbanzadeh@yahoo.com>
2023-11-26 09:33:59 +0330

    add email-service but implementation incomplete

---
+++ web-app/src/services/email-service.js
@@ -0,0 +1,11 @@
+/** @type {import("#types").EmailService} */
+export default Object.freeze({
+    /**
+     * Sends email
+     * @param {{ email: string, subject: string, body: string }} properties
+     * @returns {Promise<void>}
+     */
+    send: async function ({ email, subject, body }) {
+        /** @todo TODO */
+    }
+});
