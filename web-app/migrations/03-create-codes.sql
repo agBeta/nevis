29086b17036b472d996fbce595a102b16b8b494e
agBeta <alirezaghorbanzadeh@yahoo.com>
2023-12-01 13:22:40 +0330

    add create codes table migration

---
+++ web-app/migrations/03-create-codes.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS codes_tbl (
+    -- Column data type for hashedCode is based on https://stackoverflow.com/a/5881429.
+    hashedCode CHAR(60) PRIMARY KEY,
+    email VARCHAR(80) NOT NULL,
+    expiresAt TIMESTAMP NOT NULL
+)
+ENGINE=INNODB
+;
+
