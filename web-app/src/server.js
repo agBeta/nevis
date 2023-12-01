3c0d31160fdcfcbcb0454950928c470e01310962
agBeta <alirezaghorbanzadeh@yahoo.com>
2023-12-01 13:31:35 +0330

    update server.js and use createServer

--- web-app/src/server.js
+++ web-app/src/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { router as apiRouter } from "./routes/api/index.js";
 import { router as filesRouter } from "./routes/files-router.js";
+import { createServer } from "node:http";
 
 dotenv.config();
 const __dirname = new URL(".", import.meta.url).pathname;
@@ -12,11 +13,19 @@ app.set("trust proxy", true);
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "client"));
 
-app.use("/api", apiRouter);
-app.use("/", filesRouter);
-
-const PORT = process.env.PORT || 7198;
-
-app.listen(PORT, () => {
-    console.log(`Hello. We are live on port ${PORT}`);
+// app.use("/api", apiRouter);
+// app.use("/", filesRouter);
+app.get("/*", (req, res) => {
+    res.status(200).json({firstName: "Jack"});
 });
+
+const PORT = process.env.PORT || 9009;
+const server = createServer(app);
+
+if (process.env.NODE_ENV !== "test") {
+    server.listen(PORT, () => {
+        console.log(`Hello on port ${PORT}`);
+    });
+}
+
+export { server };
