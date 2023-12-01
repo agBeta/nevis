060b89bf395154e307301d4b10d8d7c7b95e5f28
agBeta <alirezaghorbanzadeh@yahoo.com>
2023-12-01 13:33:10 +0330

    add InvalidStateError to utils/errors.js

--- web-app/src/utils/errors.js
+++ web-app/src/utils/errors.js
@@ -7,3 +7,26 @@ export class AppError extends Error {
         Error.captureStackTrace(this, AppError);
     }
 }
+
+
+export class InvalidStateError extends Error {
+    /**
+     * This error means: Refused to continue the method. Continuing will potentially create invalid state in our objects.
+     * @param {string} message
+     */
+    constructor(message) {
+        super(message);
+        Error.captureStackTrace(this, AppError);
+    }
+}
+
+export class OperationalError extends Error {
+    /**
+     * This error is caused by external resources, e.g. database connection/query fails, HTTP request fails, etc.
+     * @param {string} message
+     */
+    constructor(message) {
+        super(message);
+        Error.captureStackTrace(this, AppError);
+    }
+}
