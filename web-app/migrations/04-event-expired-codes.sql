2486ff6c485353a8797e4f9d23ecc7ea68cf10b8
agBeta <alirezaghorbanzadeh@yahoo.com>
2023-12-01 13:23:07 +0330

    add create event for expired codes to migrations

---
+++ web-app/migrations/04-event-expired-codes.sql
@@ -0,0 +1,17 @@
+DELIMITER $$
+
+CREATE EVENT e_delete_expired_codes
+    ON SCHEDULE 
+        EVERY 1 MINUTE
+    ENABLE
+    COMMENT 'Removes codes that are expired and can no longer be used to verify the email'
+    DO
+        BEGIN
+            DELETE FROM `codes_tbl`
+            WHERE `expiresAt` < CURRENT_TIMESTAMP();
+        END$$
+
+DELIMITER ;
+
+
+--  You may checkout ALTER EVENT from docs (https://dev.mysql.com/doc/refman/8.0/en/alter-event.html).
