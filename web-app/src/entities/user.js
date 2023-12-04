73407bf6e6ad21fc3c995bd5b8a9bbafe40916c7
agBeta <alirezaghorbanzadeh@yahoo.com>
2023-12-04 09:06:34 +0330

    add user entity in user.js

--- web-app/src/entities/user.js
+++ web-app/src/entities/user.js
@@ -1,14 +1,10 @@
 import { InvalidStateError } from "#utils/errors";
 
 /**
- * @param {{
- *          Id: import("#types").IdFacility,
- *          hash: (text: string) => string,
- *          sanitize?: (text: string) => string
- * }} injections
+ * @param {{ Id: import("#types").IdFacility }} injections
  * @returns {import("#types").UserFactory}
  */
-export default function buildMakeUser({ Id, hash, sanitize }) {
+export default function buildMakeUser({ Id }) {
 
     return makeUser;
 
@@ -30,16 +26,22 @@ export default function buildMakeUser({ Id, hash, sanitize }) {
             throw new InvalidStateError("User must have a valid id.");
         }
         if (!displayName) {
-            throw new InvalidStateError("User must have a display name.");
+            throw new InvalidStateError("User must have a displayName.");
         }
         if (displayName.length < 3) {
-            throw new InvalidStateError("User displayName must be longer that 3 characters.");
+            throw new InvalidStateError("User displayName must be longer than 2 characters.");
+        }
+        if (displayName.length > 80) {
+            throw new InvalidStateError("User displayName must be smaller than 80 characters.");
         }
         if (!email) {
             throw new InvalidStateError("User must have an email");
         }
-        if (!email.includes("@") || !email.includes(".")){
+        if (!email.includes("@") || !email.includes(".")) {
             throw new InvalidStateError("User email must be valid.");
+        }
+        if (email.length > 80) {
+            throw new InvalidStateError("User email must be smaller than 80 characters.");
         }
         if (!birthYear) {
             throw new InvalidStateError("User must have a birthYear.");
@@ -47,5 +49,14 @@ export default function buildMakeUser({ Id, hash, sanitize }) {
         if (birthYear < 1200 || birthYear > 1500) {
             throw new InvalidStateError("User birthYear must greater than 1200 and less than 1500.");
         }
+
+        return Object.freeze({
+            getId: () => id,
+            getEmail: () => email,
+            getDisplayName: () => displayName,
+            getBirthYear: () => birthYear,
+            getSignupAt: () => signupAt,
+            getLastLoginAt: () => lastLoginAt
+        });
     }
 }
