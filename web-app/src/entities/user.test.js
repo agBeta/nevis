8bebbde2695aebbd4064b60c0be27cdd68bcb284
agBeta <alirezaghorbanzadeh@yahoo.com>
2023-12-04 17:00:33 +0330

    implement more test for user entity

--- web-app/src/entities/user.test.js
+++ web-app/src/entities/user.test.js
@@ -1,4 +1,4 @@
-import { describe, it } from "node:test";
+import test, { describe, it } from "node:test";
 import assert from "node:assert";
 import makeFakeUser from "../unit-fixtures/user.js";
 import { makeUser } from "./index.js";
@@ -13,12 +13,32 @@ describe("user entity", { concurrency: true }, () => {
         });
     });
 
+    it("can create an id if not supplied", (ctx) => {
+        const noIdRawUser = makeFakeUser({ id: undefined });
+        assert.doesNotThrow(() => makeUser(noIdRawUser));
+        const user = makeUser(noIdRawUser);
+        assert.strictEqual(typeof user.getId() === "string", true);
+        // ctx.diagnostic(user.getId());
+    });
+
+    it("can have an id", () => {
+        const rawUser = makeFakeUser({});
+        assert.doesNotThrow(() => makeUser(rawUser));
+    });
+
+    it("should throw error if given id isn't valid", (ctx) => {
+        //  Note, isCuid has some issues at the moment (2023-12). See https://github.com/paralleldrive/cuid2/issues/66.
+        const rawUser = makeFakeUser({ id: "invalid id" });
+        assert.throws(() => makeUser(rawUser), {
+            name: /StateError/i,
+            message: /must have a valid id/i
+        });
+    });
+
+    it.todo("signupAt is now in UTC if signupAt isn't given");
+
     it.todo("should throw error if email isn't valid");
     it.todo("should throw error if birthYear isn't valid");
-
-    it.todo("should throw error if given id isn't valid");
-    it.todo("should create id if id isn't given");
-    it.todo("signupAt is now in UTC if signupAt isn't given");
     it.todo("lastLoginAt is now in UTC if lastLoginAt isn't given");
 });
 
