ad8039942b7bb16731a56343fe0b6ec74519f51f
agBeta <alirezaghorbanzadeh@yahoo.com>
2023-12-13 16:52:54 +0330

    integrate log with morgan

--- web-app/src/server.js
+++ web-app/src/server.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import { router as apiRouter } from "./routes/api-router.js";
 import { router as filesRouter } from "./routes/files-router.js";
 import morgan from "morgan";
+import log from "#utils/log.js";
 
 dotenv.config();
 const __dirname = new URL(".", import.meta.url).pathname;
@@ -14,7 +15,36 @@ app.set("trust proxy", true);
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "client"));
 
-app.use(morgan("dev"));
+
+app.use(morgan(
+    function (tokens, req, res) {
+        return [
+            tokens.method(req, res),
+            tokens.url(req, res), " ---",
+            tokens.status(req, res), "-",
+            (tokens.res(req, res, "content-length") ?? "0").concat(" bytes -"),
+            (tokens["response-time"](req, res) ?? "0").concat(" ms")
+        ].join(" ");
+    },
+    {
+        //  According to the comment by Devon Sams in https://stackoverflow.com/a/28824464/22969951 and
+        //  https://github.com/expressjs/morgan#morganformat-options.
+        stream: {
+            write: function (str) {
+                log({
+                    level: "http",
+                    keyword: "morgan",
+                    // to remove trailing \n
+                    message: str.slice(0, -1)
+                });
+            }
+        },
+        immediate: false,
+        skip: function (req, res) { return res.statusCode >= 500; }
+    })
+);
+
+
 app.use("/api", apiRouter);
 app.use("/", filesRouter);
 
