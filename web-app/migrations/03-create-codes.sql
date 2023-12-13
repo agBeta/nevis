6b1dae7eaa293257972e2a67fac1410f76858eb6
agBeta <alirezaghorbanzadeh@yahoo.com>
2023-12-13 23:34:14 +0330

    remove ENUM from create codes migration

--- web-app/migrations/03-create-codes.sql
+++ web-app/migrations/03-create-codes.sql
@@ -1,8 +1,10 @@
 CREATE TABLE IF NOT EXISTS codes_tbl (
     hashedCode CHAR(60) PRIMARY KEY,
     email VARCHAR(80) NOT NULL,
-    purpose ENUM('signup', 'reset-password'),
-    expiresAt TIMESTAMP NOT NULL
+    purpose VARCHAR(20) NOT NULL,
+    expiresAt TIMESTAMP NOT NULL,
+    CONSTRAINT chk_codes_purpose CHECK (purpose = 'signup' OR purpose = 'reset-pass'),
+    INDEX idx_codes_email (email)
 )
 ENGINE=INNODB
 ;
