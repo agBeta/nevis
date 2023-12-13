4c99a045cd156328bf61af8d374c01eaf560bb53
agBeta <alirezaghorbanzadeh@yahoo.com>
2023-12-13 12:00:57 +0330

    add http-client fixture

---
+++ web-app/tests/fixtures/http-client.js
@@ -0,0 +1,16 @@
+const PORT = process.env.PORT;
+const BASE_URL = `http://localhost:${PORT}`;
+
+export function postRequest(/** @type {string} */ url, /** @type {any} */ body) {
+    if (!url) {
+        throw new Error("postRequest must have a url.");
+    }
+    if (!url.startsWith("/")) {
+        throw new Error("postRequest url must start with slash(/).");
+    }
+
+    return fetch(BASE_URL + url, {
+        method: "POST",
+        body: JSON.stringify(body)
+    });
+}
