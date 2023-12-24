ae8429640f309186d75f3e0186d0966886f1cd78
agBeta <alirezaghorbanzadeh@yahoo.com>
2023-12-24 09:28:26 +0330

    remove action routes from auth-router, add login and authenticated_as

--- web-app/src/routes/auth-router.js
+++ web-app/src/routes/auth-router.js
@@ -2,9 +2,9 @@ import express from "express";
 import makeExpressCallback from "../express-stuff/express-callback.js";
 import {
     auth_code_POST,
-    generic_action_creation_POST,
-    generic_action_status_GET,
-    auth_signup_action_PUT,
+    auth_login_POST,
+    auth_signup_POST,
+    auth_authenticated_as_GET,
 } from "#controllers";
 
 const router = express.Router();
@@ -12,12 +12,10 @@ const router = express.Router();
 // middleware
 router.use("/", express.json({ limit: "20kb" }));
 
-
 router.post("/code", makeExpressCallback(auth_code_POST));
-
-router.post("/signup", makeExpressCallback(generic_action_creation_POST));
-router.put("/signup/action/:actionId", makeExpressCallback(auth_signup_action_PUT));
-router.get("/signup/action/:actionId", makeExpressCallback(generic_action_status_GET));
+router.post("/signup", makeExpressCallback(auth_signup_POST));
+router.post("/login", makeExpressCallback(auth_login_POST));
+router.get("/authenticated_as", makeExpressCallback(auth_authenticated_as_GET));
 
 
 export { router };
