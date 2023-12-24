dfafb321a1141dfd2146e805e9e9341e5e13f6da
agBeta <alirezaghorbanzadeh@yahoo.com>
2023-12-24 09:17:04 +0330

    add (almost) empty crud test files to data-access

---
+++ web-app/src/data-access/session-crud.test.js
@@ -0,0 +1,3 @@
+//  all tests related to insert, delete and retrieval of a session. including cache.
+//  it is important. because insert_session might not work properly with
+//  find_from session (especially in timestamp field).
