bfd30b0c88e5eb872dbd58739263715ddb4abb42
agBeta <alirezaghorbanzadeh@yahoo.com>
2023-12-24 12:56:55 +0330

    add empty (comments only) mysql2.js to playground

---
+++ web-app/playground/mysql2.js
@@ -0,0 +1,9 @@
+// play with error object
+// error when username doesn't exist
+// error when UNQ is violated
+// error when foreign key constraint is violated
+// error when type casting string to number happen
+// error when CHK is violated
+
+// play with timestamp and number (expiresAt)
+// play with date
