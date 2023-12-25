3ce191de6a26401eafd2d2fcb5d0b7de6cf13f7f
agBeta <alirezaghorbanzadeh@yahoo.com>
2023-12-25 09:42:10 +0330

    add image_url to blog sql migration

--- migrations/02-create-blog.sql
+++ migrations/02-create-blog.sql
@@ -8,6 +8,7 @@ CREATE TABLE IF NOT EXISTS blog_tbl (
     blog_title VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
     blog_body TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
     blog_topic VARCHAR(63) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+    image_url VARCHAR(255) NULL,
     
     is_published TINYINT(1) NOT NULL DEFAULT 0,
     created_at TIMESTAMP NOT NULL,
