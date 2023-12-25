a350a0f46800ce34d1354d57b53485cfddab6236
agBeta <alirezaghorbanzadeh@yahoo.com>
2023-12-25 18:07:21 +0330

    update blog-router, add more routes

--- web-app/src/routes/blog-router.js
+++ web-app/src/routes/blog-router.js
@@ -1,15 +1,31 @@
 import express from "express";
 import makeExpressCallback from "../express-stuff/express-callback";
-
+import requireAuthentication from "../express-stuff/auth-middleware.js";
+import {
+    blog_POST_action_creation,
+    blog_action_PUT,
+    blog_blogId_GET,
+    blog_paginated_GET,
+    blog_GET,
+} from "#controllers";
 
 const router = express.Router();
 
-router.use("/", express.json({
-    // How? See json-size-kb.js in playground.
-    limit: "50kb"
-}));
+router.use("/", express.json({ limit: "10kb" }));
 
 // (semi-?)idempotent POST
-router.post("/", makeExpressCallback())
+router.post("/",
+    requireAuthentication,
+    makeExpressCallback(blog_POST_action_creation)
+);
+
+router.put("/action/:actionId",
+    requireAuthentication,
+    makeExpressCallback(blog_action_PUT)
+);
+
+router.get("/:blogId", makeExpressCallback(blog_blogId_GET));
+router.get("/", makeExpressCallback(blog_GET));
+router.get("/paginated", makeExpressCallback(blog_paginated_GET));
 
 export { router };
