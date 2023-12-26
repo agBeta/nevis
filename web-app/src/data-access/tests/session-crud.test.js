93400457b590bef0b4c4f77ff480487f0b6f8811
agBeta <alirezaghorbanzadeh@yahoo.com>
2023-12-26 16:16:45 +0330

    change some test env files

--- web-app/src/data-access/tests/session-crud.test.js
+++ web-app/src/data-access/tests/session-crud.test.js
@@ -3,16 +3,16 @@ import { test } from "node:test";
 import assert from "node:assert";
 import dotenv from "dotenv";
 
-import make_find_session_record_by_hashedSessionId from "./find_session_record_by_hashedSessionId.js";
-import makeDbConnectionPool from "./connection.js";
-import makeRedisClient from "./cache-connection.js";
+import makeDbConnectionPool from "../connection.js";
+import makeRedisClient from "../cache-connection.js";
+import make_find_session_record_by_hashedSessionId from "../find_session_record_by_hashedSessionId.js";
 
 dotenv.config({
-    path: path.resolve(new URL(".", import.meta.url).pathname, "test-configs", "session-CRUD.env"),
-    override: true
+    path: path.resolve(new URL(".", import.meta.url).pathname, "session-crud.env"),
+    override: true,
 });
 
-const dbConnectionPool = makeDbConnectionPool({ port: 3306 });
+const dbConnectionPool = makeDbConnectionPool({ port: process.env.MYSQL_PORT });
 const find_session_record_by_hashedSessionId = make_find_session_record_by_hashedSessionId({
 
 });
