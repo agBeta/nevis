8d6a3c364a30e0af5cf7e734bab8bd0f4fc6164c
agBeta <alirezaghorbanzadeh@yahoo.com>
2023-12-27 08:21:15 +0330

    add guard to check signupAt is number inside insert_user

--- web-app/src/data-access/insert_user.js
+++ web-app/src/data-access/insert_user.js
@@ -1,4 +1,4 @@
-import { InvalidError, OperationalError } from "#utils/errors.js";
+import { AppError, InvalidError, OperationalError } from "#utils/errors.js";
 
 /**
  * @param {{ dbConnectionPool: MySQLConnectionPool}} props
@@ -26,6 +26,19 @@ export default function make_insert_user({ dbConnectionPool }) {
     /**@type {Insert_User}*/
     async function insert_user({ id, email, hashedPassword, displayName, birthYear, signupAt }) {
         try {
+            //  To enforce consistency, consumer of this function must specify signupAt as timestamp (i.e. number).
+            //  Although TS helps us catch type-related bugs, but we add this check to be sure in runtime.
+            if (typeof signupAt !== "number") {
+                // Why AppError? To appear in logs. Anyway...
+                throw new AppError("[signupAt] must be timestamp in milliseconds, not Date.", "db__invalid_timestamp");
+            }
+            //  Also timestamp must be in milliseconds (not in seconds). If the consumer incorrectly gives a timestamp
+            //  in seconds, then the corresponding date of given timestamp would be something near 1970, way before
+            //  2000s.
+            if (signupAt < new Date("1990-01-01T00:00:00+00:00").getTime()) {
+                throw new AppError("[signupAt] must be timestamp in milliseconds, not Date.", "db__invalid_timestamp");
+            }
+
             const db = await dbConnectionPool;
             await db.execute(sqlCmd, [
                 id,
@@ -37,6 +50,9 @@ export default function make_insert_user({ dbConnectionPool }) {
             ]);
         }
         catch (error) {
+            if (error instanceof AppError){
+                throw error; // bubble to upstream
+            }
             if (error?.sqlMessage?.includes?.("UNQ_user_email")){
                 throw new InvalidError("Email already exists.");
             }
