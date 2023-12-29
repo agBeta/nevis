99975de651e6d210024dec3087f101f5fe2206b9
agBeta <alirezaghorbanzadeh@yahoo.com>
2023-12-29 19:21:11 +0330

    still trouble with cookie, test is failing (bad commit, backup)

--- web-app/tests/fixtures/http-client.js
+++ web-app/tests/fixtures/http-client.js
@@ -1,10 +1,5 @@
-import { Client } from "undici";
 import makeFetchCookie from "fetch-cookie";
 
-const fetchCookie = makeFetchCookie(fetch, new makeFetchCookie.toughCookie.CookieJar(undefined, {
-    allowSpecialUseDomain: true,
-    prefixSecurity: "strict"
-}));
 
 /**
  * @param {{ port: number }} props
@@ -14,50 +9,49 @@ export default function makeHttpClient({ port }) {
         throw new Error("port is undefined or null.");
     }
     const BASE_URL = `http://localhost:${port}`;
-    const client = new Client(BASE_URL);
-    let data = []
+
+    // https://github.com/salesforce/tough-cookie?tab=readme-ov-file#cookiejarstore-options
+    const cookieJar = new makeFetchCookie.toughCookie.CookieJar(
+        // https://github.com/salesforce/tough-cookie?tab=readme-ov-file#store
+        undefined,
+        { allowSpecialUseDomain: true, prefixSecurity: "strict" }
+    );
+    const fetchCookie = makeFetchCookie(fetch, cookieJar);
 
     return Object.freeze({
         postRequest,
         getRequest,
     });
 
-    function postRequest(/**@type {string}*/ url, /**@type {any}*/ body) {
+    async function postRequest(/**@type {string}*/ url, /**@type {any}*/ body) {
         if (!url) {
             throw new Error("postRequest must have a url.");
         }
         if (!url.startsWith("/")) {
             throw new Error("postRequest url must start with slash(/).");
         }
-        data = [];
-        // https://github.com/nodejs/undici/blob/main/docs/api/Dispatcher.md#example-3---dispatch-post-request
-        return client.dispatch({
+        const raw = await fetchCookie(BASE_URL + url, {
             method: "POST",
             // Headers are crucial. Never omit them.
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(body),
-            path: url,
-        }, {
-            onConnect: () => {
-                console.log('Connected!')
-            },
-            onError: (error) => {
-                console.error(error)
-            },
-            onHeaders: (statusCode, headers) => {
-                console.log(`onHeaders | statusCode: ${statusCode} | headers: ${headers}`)
-            },
-            onData: (chunk) => {
-                console.log('onData: chunk received')
-                data.push(chunk)
-            },
-            onComplete: (trailers) => {
-                console.log(`onComplete | trailers: ${trailers}`)
-                const res = Buffer.concat(data).toString('utf8');
-                console.log(`Response Data: ${res}`);
-            }
+            body: JSON.stringify(body)
         });
+        for (let coo of raw.headers.getSetCookie()) {
+            //  We must add "Secure" when a cookie has __Host- prefix. Otherwise toughCookie will throw
+            //  an error. See https://github.com/salesforce/tough-cookie?tab=readme-ov-file#cookie-prefixes.
+            //  Recall, the server (i.e. login controller) won't set secure option for cookies when
+            //  NODE_ENV=test.
+            // coo += "; Secure";
+            console.log(coo);
+            await cookieJar.setCookie(coo, BASE_URL); // ---> not working correctly. don't know why?
+        }
+        cookieJar.setCookie(raw.headers.getSetCookie()[0].split(";")[0], BASE_URL);
+        cookieJar.setCookie(raw.headers.getSetCookie()[1].split(";")[0], BASE_URL);
+        cookieJar.setCookie("foo=base", BASE_URL + url);
+        console.log(cookieJar.getCookiesSync(BASE_URL));
+        return raw.clone();
     }
+
 
     async function getRequest(/**@type {string}*/ url) {
         if (!url) {
@@ -66,25 +60,12 @@ export default function makeHttpClient({ port }) {
         if (!url.startsWith("/")) {
             throw new Error("getRequest url must start with slash(/).");
         }
-        const {
-            statusCode,
-            headers,
-            trailers,
-            body
-        } = await client.request({
-            path: url,
-            method: 'GET',
-        });
-
-        console.log('response received', statusCode)
-        console.log('headers', headers)
-
-        for await (const data of body) {
-            console.log('data', data)
-        }
-
-        console.log('trailers', trailers)
+        console.log(" cookies in getRequest ", " 🗒️ ".repeat(10));
+        console.log(cookieJar.getCookiesSync(BASE_URL));
+        const raw = await fetchCookie(BASE_URL + url);
+        return raw.clone();
     }
+
 }
 
 
