d4ee0094ecef4319e9d4dcf51bf8c965a7a06074
agBeta <alirezaghorbanzadeh@yahoo.com>
2024-01-04 20:20:28 +0330

    add ui utils containing showHideLoadingSpinner, createErrorElement

---
+++ web-app/src/client/public/js/ui-utils.js
@@ -0,0 +1,34 @@
+/** @param {HTMLElement} insideEl @param {boolean} visibility  */
+export function showHideLoadingSpinner(insideEl, visibility) {
+    if (visibility) {
+        insideEl.setAttribute("aria-busy", "true");
+        insideEl.innerHTML += /*html*/`
+            <div class="loading-container">
+                <span class="loading-spinner"></span>
+            </div>
+        `;
+    } else {
+        insideEl.setAttribute("aria-busy", "false");
+        const loaderEl = insideEl.querySelector(".loading-container");
+        loaderEl?.remove();
+    }
+}
+
+
+export function createErrorElement({ title, description, buttonTitle, onButtonClick }) {
+    const errorEl = document.createElement("div");
+    errorEl.innerHTML = /*html*/`
+        <h2 class="error-title">${title}</h2>
+        <p>${description}</p>
+    `;
+    errorEl.classList.add("error-container");
+    if (buttonTitle) {
+        const button = document.createElement("button");
+        button.textContent = buttonTitle;
+        button.addEventListener("click", onButtonClick);
+        errorEl.appendChild(button);
+    }
+    return errorEl;
+}
+
+//
