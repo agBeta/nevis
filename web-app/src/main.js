487b3efbfb502060f2814416f2c19c5fbb486086
agBeta <alirezaghorbanzadeh@yahoo.com>
2024-01-04 12:48:50 +0330

    install blogRouter on main.js

--- web-app/src/main.js
+++ web-app/src/main.js
@@ -5,6 +5,7 @@ import helmet from "helmet";
 import "./config.js";
 import { installRouter, installMorgan, makeExpressApp } from "./express-stuff/server.js";
 import { router as authRouter } from "./routes/auth-router.js";
+import { router as blogRouter } from "./routes/blog-router.js";
 import { router as filesRouter } from "./routes/files-router.js";
 
 const __dirname = new URL(".", import.meta.url).pathname;
@@ -21,6 +22,7 @@ app.use(helmet({
 
 installMorgan({ app });
 installRouter({ app, router: authRouter, pathPrefix: "/api/v1/auth" });
+installRouter({ app, router: blogRouter, pathPrefix: "/api/v1/blog" });
 
 app.use("/", filesRouter);
 
