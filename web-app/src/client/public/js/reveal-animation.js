0db94bbde2d6230e8475e19e64f785d15653f529
agBeta <alirezaghorbanzadeh@yahoo.com>
2024-01-04 08:09:47 +0330

    change reveal animation parts, change css classes, html elements, js function names

--- web-app/src/client/public/js/reveal-animation.js
+++ web-app/src/client/public/js/reveal-animation.js
@@ -1,59 +1,69 @@
-const wraps = [...document.querySelectorAll(".wrap")];
-const menuToggle = document.querySelector(".menu-toggle");
-const menu = document.querySelector(".menu");
-const menuWraps = [...document.querySelectorAll(".menu-wrap")];
-
-menuToggle?.addEventListener("click", () => {
-    if (menu?.classList.contains("active")) {
-        menuToggle.classList.remove("active");
-        toggleMenuWraps(false);
-        setTimeout(() => {
-            menu.classList.remove("active");
-        }, 300);
-        setTimeout(() => {
-            toggleWraps(true);
-        }, 300);
-    }
-    else {
-        menuToggle.classList.add("active");
-        toggleWraps(false);
-        setTimeout(() => {
-            menu.classList.add("active");
-        }, 300);
-        setTimeout(() => {
-            toggleMenuWraps(true);
-        }, 300);
-    }
+document.addEventListener("DOMContentLoaded", () => {
+    handleRevealAnimation();
 });
 
-function toggleWraps(active) {
-    wraps.forEach(wrap => {
-        toggleWrap(wrap, active);
-    });
+
+export default function handleRevealAnimation() {
+    const panelElementsToReveal = /**@type {HTMLElement[]}*/
+        ([...document.querySelectorAll(".to-reveal:not(.nav-item)")]);
+    const menuElementsToReveal = /**@type {HTMLElement[]}*/
+        ([...document.querySelectorAll("nav[aria-label='Main Menu'] .to-reveal")]);
+    const menuToggle = /**@type {HTMLElement}*/(document.querySelector(".menu-toggle"));
+    const menu = /**@type {HTMLElement}*/(document.querySelector("nav[aria-label='Main Menu']"));
+
+    menuToggle.addEventListener("click", onMenuToggleClick);
+    revealAllPanelElements();
+
+
+    /** @param {HTMLElement} wrap  @param {boolean} active  */
+    function toggleReveal(wrap, active) {
+        requestAnimationFrame(() => {
+            if (active) {
+                wrap.classList.add("active");
+            }
+            else {
+                wrap.classList.remove("active");
+            }
+        });
+    }
+
+    function revealAllPanelElements() {
+        panelElementsToReveal.forEach((wrap, i) => {
+            setTimeout(() => {
+                wrap.classList.add("active");
+            }, (i + 1) * 50);
+        });
+    }
+
+    function onMenuToggleClick() {
+        if (menu.classList.contains("active")) {
+            menuToggle.classList.remove("active");
+            menuElementsToReveal.forEach(wrap => {
+                toggleReveal(wrap, false);
+            });
+            setTimeout(() => { menu.classList.remove("active"); }, 200);
+            setTimeout(() => {
+                panelElementsToReveal.forEach(wrap => {
+                    toggleReveal(wrap, true);
+                });
+            }, 200);
+        }
+        else {
+            menuToggle.classList.add("active");
+            panelElementsToReveal.forEach(wrap => {
+                toggleReveal(wrap, false);
+            });
+
+            setTimeout(() => {
+                menu.classList.add("active");
+            }, 300);
+            setTimeout(() => {
+                menuElementsToReveal.forEach(wrap => {
+                    toggleReveal(wrap, true);
+                });
+            }, 300);
+        }
+
+    }
 }
 
-function toggleMenuWraps(active) {
-    menuWraps.forEach(wrap => {
-        toggleWrap(wrap, active);
-    });
-}
-
-function toggleWrap(wrap, active) {
-    setTimeout(() => {
-        if (active)
-            wrap.classList.add("active");
-        else
-            wrap.classList.remove("active");
-    });
-}
-
-function displayWraps() {
-    wraps.forEach((wrap, i) => {
-        setTimeout(() => {
-            wrap.classList.add("active");
-        }, (i + 1) * 50);
-    });
-}
-
-
-displayWraps();
