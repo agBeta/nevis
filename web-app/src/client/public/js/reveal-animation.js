afab13e2c47dab267f7b40b0ca7c0068ebf8be90
agBeta <alirezaghorbanzadeh@yahoo.com>
2024-01-04 20:21:01 +0330

    change reveal-animation and only export onMenuToggleClick

--- web-app/src/client/public/js/reveal-animation.js
+++ web-app/src/client/public/js/reveal-animation.js
@@ -1,47 +1,40 @@
-document.addEventListener("DOMContentLoaded", () => {
 
-    const panelElementsToReveal = /**@type {HTMLElement[]}*/
-        ([...document.querySelectorAll(".to-reveal:not(.nav-item)")]);
-    const menuElementsToReveal = /**@type {HTMLElement[]}*/
-        ([...document.querySelectorAll("nav[aria-label='Main Menu'] .to-reveal")]);
+export function onMenuToggleClick() {
     const menuToggle = /**@type {HTMLElement}*/(document.querySelector(".menu-toggle"));
     const menu = /**@type {HTMLElement}*/(document.querySelector("nav[aria-label='Main Menu']"));
 
-    menuToggle.addEventListener("click", onMenuToggleClick);
-    // Reveal menu by default when the page loads.
-    menuToggle.click();
+    const panelElementsToReveal = /**@type {NodeListOf<HTMLElement>}*/
+        (document.querySelectorAll(".to-reveal:not(.nav-item)"));
+    const menuElementsToReveal = /**@type {NodeListOf<HTMLElement>}*/
+        (document.querySelectorAll("nav[aria-label='Main Menu'] .to-reveal"));
 
-    function onMenuToggleClick() {
-        if (menu.classList.contains("active")) {
-            menuToggle.classList.remove("active");
-            menuToggle.setAttribute("aria-expanded", "false");
-            menuElementsToReveal.forEach(wrap => {
-                toggleReveal(wrap, false);
-            });
-            menu.classList.remove("active");
-            panelElementsToReveal.forEach(wrap => {
-                toggleReveal(wrap, true);
-            });
-        }
-        else {
-            menuToggle.classList.add("active");
-            menuToggle.setAttribute("aria-expanded", "true");
-            panelElementsToReveal.forEach(wrap => {
-                toggleReveal(wrap, false);
-            });
-            menu.classList.add("active");
-            menuElementsToReveal.forEach(wrap => {
-                toggleReveal(wrap, true);
-            });
-        }
-
-        //  You may also add aria-hidden="true" to nav-items for screen readers when menu isn't expanded. We didn't
-        //  do that.
-        //  Some useful links:
-        //  https://www.linkedin.com/pulse/hiding-elements-from-screen-readers-girijesh-tripathi.
-        //  https://snook.ca/archives/html_and_css/hiding-content-for-accessibility.
+    if (menu.classList.contains("active")) {
+        menuToggle.classList.remove("active");
+        menuToggle.setAttribute("aria-expanded", "false");
+        menuElementsToReveal.forEach(wrap => {
+            toggleReveal(wrap, false);
+        });
+        menu.classList.remove("active");
+        panelElementsToReveal.forEach(wrap => {
+            toggleReveal(wrap, true);
+        });
     }
-});
+    else {
+        menuToggle.classList.add("active");
+        menuToggle.setAttribute("aria-expanded", "true");
+        panelElementsToReveal.forEach(wrap => {
+            toggleReveal(wrap, false);
+        });
+        menu.classList.add("active");
+        menuElementsToReveal.forEach(wrap => {
+            toggleReveal(wrap, true);
+        });
+    }
+    //  You may also add aria-hidden="true" to nav-items for screen readers when menu isn't expanded. We didn't
+    //  do that. Some useful links:
+    //  https://www.linkedin.com/pulse/hiding-elements-from-screen-readers-girijesh-tripathi.
+    //  https://snook.ca/archives/html_and_css/hiding-content-for-accessibility.
+}
 
 
 /** @param {HTMLElement} el  @param {boolean} active  */
