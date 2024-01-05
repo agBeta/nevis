885d26c13b272a8e55869e47f1537217403af941
agBeta <alirezaghorbanzadeh@yahoo.com>
2024-01-05 19:39:43 +0330

    fix bug in router

--- web-app/src/client/public/js/router.js
+++ web-app/src/client/public/js/router.js
@@ -48,7 +48,7 @@ export default function makeRouter({ routes }) {
             if (ev.target.matches("[data-link]")) {
                 // We aren't letting the browser take care of navigation, since we want to do it by our router, so...
                 ev.preventDefault();
-                navigateTo(/**@type {HTMLAnchorElement}*/(ev.target).pathname, true);
+                navigateTo(/**@type {HTMLAnchorElement}*/(ev.target).href, true);
             }
         });
 
