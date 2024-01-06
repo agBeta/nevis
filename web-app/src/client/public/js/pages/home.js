04ec2bfdd8eae90b1225faf8d46c549d5ac17f45
agBeta <alirezaghorbanzadeh@yahoo.com>
2024-01-06 13:45:42 +0330

    add home page view

---
+++ web-app/src/client/public/js/pages/home.js
@@ -0,0 +1,43 @@
+import { toggleRevealOfPageElements } from "../reveal-animation.js";
+
+/** @returns {PageView} */
+export default function makeHomeView() {
+    const pageEl = /**@type {HTMLElement}*/ (document.querySelector("#page"));
+    const THIS_VIEW = "home_view";
+
+    return Object.freeze({
+        render,
+        NAME: THIS_VIEW,
+    });
+
+    async function render() {
+        document.title = "خانه";
+        pageEl.innerHTML = "";
+        // console.log("Here");
+        document.querySelectorAll("nav[aria-label='Main Menu'] .nav-item > a").forEach(el => {
+            // @ts-ignore
+            if (el.pathname === "/") el.setAttribute("aria-current", "page");
+            else el.removeAttribute("aria-current");
+        });
+
+        const containerEl = document.createElement("div");
+        containerEl.classList.add("to-reveal");
+        // containerEl.setAttribute("aria-hidden", "false");
+        containerEl.id = "home";
+        containerEl.innerHTML = /*html*/`
+            <h1 class="h1">خانه</h1>
+            <p dir="rtl">خوش آمدید!</p>
+            <p dir="rtl">
+            اسامی تمامی کاربران و همچنین عناوین و مطالب همه بلاگ‌ها مصنوعی بوده و توسط پکیج
+                <span class="en" dir="ltr">Faker</span>
+                تولید و به دیتابیس اضافه شده است.
+            </p>
+        `;
+        pageEl.appendChild(containerEl);
+        toggleRevealOfPageElements(true);
+    }
+}
+
+/**
+ * @typedef {import("../types.d.ts").PageView} PageView
+ */
