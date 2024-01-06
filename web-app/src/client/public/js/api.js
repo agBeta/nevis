496c22492b9e16a19733951fee57f2147b94fcf4
agBeta <alirezaghorbanzadeh@yahoo.com>
2024-01-06 20:07:11 +0330

    add postEmailForCode and postSignup in api.js

--- web-app/src/client/public/js/api.js
+++ web-app/src/client/public/js/api.js
@@ -17,7 +17,7 @@ export async function fetchBlogPaginated({ direction, cursor, limit }) {
     const raw = await fetch(request);
 
     const statusCode = raw.status;
-    // You can be sure status 204 isn't returned so there must be a body.
+    // You can be sure status 204 isn't returned so there will always be a "body".
     const body = await raw.json();
     return {
         statusCode,
@@ -25,7 +25,7 @@ export async function fetchBlogPaginated({ direction, cursor, limit }) {
     };
 }
 
-/** @param {{ blogId: string }} param0 */
+/** @type {import("./types.d.ts").FetchBlog} */
 export async function fetchBlog({ blogId }) {
     const url = new URL(`/api/v1/blog/${blogId}`, BASE_URL);
     const request = new Request(url, {
@@ -41,4 +41,43 @@ export async function fetchBlog({ blogId }) {
         body,
     };
 }
+
+/** @type {import("./types.d.ts").PostEmailForCode} */
+export async function postEmailForCode({ email, purpose }) {
+    const url = new URL("/api/v1/auth/code", BASE_URL);
+    const raw = await fetch(url, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, purpose }),
+    });
+    const statusCode = raw.status;
+    const body = await raw.json();
+    return {
+        statusCode,
+        body,
+    };
+}
+
+/** @type {import("./pages/signup.js").PostSignup} */
+export async function postSignup({ email, displayName, password, repeatPassword, birthYear, code }) {
+    const url = new URL("/api/v1/auth/signup", BASE_URL);
+    const raw = await fetch(url, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, displayName, password, repeatPassword, birthYear, code }),
+    });
+    const statusCode = raw.status;
+    const body = await raw.json();
+    return {
+        statusCode,
+        body,
+    };
+}
+//
 
