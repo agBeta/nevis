c04dc937cd9674cbd31872c2d985ca1caf4224ba
agBeta <alirezaghorbanzadeh@yahoo.com>
2024-01-08 09:40:26 +0330

    add a bit to one test in basic-navigation

--- web-app/e2e/tests/basic-navigation.spec.js
+++ web-app/e2e/tests/basic-navigation.spec.js
@@ -8,9 +8,12 @@ test("has title", async ({ page }) => {
 test("has correct on /blog/paginated", async({ page }) => {
     await page.goto("/blog/paginated");
     await expect(page).toHaveTitle(/نوشته‌ها/);
+
+    await page.goto("/blog/paginated?limit=20&direction=newer&cursor=oldest");
+    await expect(page).toHaveTitle(/نوشته‌ها/);
 } );
 
-test.only("doesn't break when we have a hash (#) at the end of url", async({ page }) => {
+test("doesn't break when we have a hash (#) at the end of url", async({ page }) => {
     //  Don't test goto("/"), because most of the time, when routing fails we redirect
     //  to homepage. So it's safer to test it on another page. So...
     await page.goto("/signup#some_hash");
