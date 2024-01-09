ed87418310eb0e859f094d0359e812a5e2c490f7
agBeta <alirezaghorbanzadeh@yahoo.com>
2024-01-09 11:55:45 +0330

    add few more console logs to global-setup

--- web-app/e2e/tests/global-setup.spec.js
+++ web-app/e2e/tests/global-setup.spec.js
@@ -2,7 +2,9 @@ import { clearAll, seedUsers, seedBlogs, dbConnectionPool } from "../utils/seed.
 
 console.log("📢 ----- Setup ------");
 await clearAll();
+console.log("Seeding users");
 const userIds = await seedUsers(5, /*saveAsFixtureFile=*/true);
+console.log("Seeding blogs");
 await seedBlogs(userIds, 50);
 
 await dbConnectionPool.end();
