60e021d615c9d5b42ee3b69cc63e0031e8e134ed
agBeta <alirezaghorbanzadeh@yahoo.com>
2024-01-09 13:24:28 +0330

    add getSomeRealUsers in test-utils

--- web-app/e2e/utils/test-utils.js
+++ web-app/e2e/utils/test-utils.js
@@ -1,7 +1,31 @@
+import path from "node:path";
+import fs from "node:fs";
+
+const pathToUsersFixture = path.resolve(
+    new URL(".", import.meta.url).pathname, "..", "fixtures", "users.json"
+);
+
 /**
  * @param {number} [n=1]
+ * @returns {Array<{
+ *      id: string,
+ *      email: string,
+ *      password: string,
+ *      displayName: string,
+ *      birthYear: 1364,
+ *      signupAt: Date,
+ * }>}
  */
 export function getSomeRealUsers(n = 1){
+    const allUsersFixtures = JSON.parse(fs.readFileSync(pathToUsersFixture, "utf-8"));
+    // @ts-ignore
+    const users = allUsersFixtures.slice(0, n).map(user => {
+        return {
+            ...user,
+            signupAt: new Date(user.signupAt), // convert string to native JS Date object
+        };
+    });
+    return users.slice(0, n);
 }
 
 // maybe functions like find_user, etc, to interact with db
